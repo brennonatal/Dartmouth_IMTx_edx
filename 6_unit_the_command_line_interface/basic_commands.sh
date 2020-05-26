7bb98575b5747db8ba055b96c3d05247897c4b02
Brenno Natal <natalbrenno@gmail.com>
2020-05-26 09:43:39 -0300

    Getting started with unit 6

---
+++ 6_unit_the_command_line_interface/basic_commands.sh
@@ -0,0 +1,26 @@
+#comment 
+echo "Hello world" #print something to screen
+
+pwd #show the path to the directory you are located
+
+man [command] #manual to use command
+
+whoami #display username
+
+id #display more detailed content about user
+
+logname #writes the user's login name to standard output
+
+hostname #print name of current host system
+
+uname #rint operating system name
+
+uptime #9:39  up 33 mins, 1 user, load averages: 2.38 2.87 3.40
+
+cal #display calendar
+
+date #print date
+
+date +"%T" #show time
+
+date +"%A %d %B %Y" #Tuesday 26 May 2020
