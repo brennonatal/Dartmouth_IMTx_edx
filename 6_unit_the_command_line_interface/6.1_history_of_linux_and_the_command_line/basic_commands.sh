8a3c1f0dece92838c0946aff0a633c975c169ffd
Brenno Natal <natalbrenno@gmail.com>
2020-07-08 22:34:20 -0300

    Reorganizing unit 6.1

--- 6_unit_the_command_line_interface/6.1_history_of_linux_and_the_command_line/basic_commands.sh
+++ 6_unit_the_command_line_interface/6.1_history_of_linux_and_the_command_line/basic_commands.sh
@@ -1,7 +1,7 @@
 #comment 
 echo "Hello world" #print something to screen
 
-pwd #show the path to the directory you are located
+pwd #stands for "print working directory"
 
 man [command] #manual to use command
 
@@ -11,9 +11,9 @@ id #display more detailed content about user
 
 logname #writes the user's login name to standard output
 
-hostname #print name of current host system
+hostname #display name of current host system
 
-uname #rint operating system name
+uname #display operating system name
 
 uptime #9:39  up 33 mins, 1 user, load averages: 2.38 2.87 3.40
 
